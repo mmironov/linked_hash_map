8ee39d29a0cde16adf7a3b88493785439cb745ac
Miroslav <miroslavmironov@gmail.com>
2018-04-16 17:15:24 +0300

    Cleaan up the project and prepared it for the Fundamentals Data Structures part 2 trainings.

--- src/com/musala/LinkedHashMap.java
+++ src/com/musala/LinkedHashMap.java
@@ -6,193 +6,38 @@ import java.util.LinkedList;
 
 //TODO: Implement a LinkedHashMap that keeps the order of insertion of the elements
 public class LinkedHashMap<K, V> implements Map<K, V> {
-
-	private static final int SIZE = 11;
-	
-	private ArrayList<LinkedList<Pair<K, V>>> table;
-	private int size;
-
-	private Pair<K, V> first;
-	private Pair<K, V> last;
-
-	public LinkedHashMap() {
-		table = new ArrayList<>(SIZE);
-		
-		for(int i=0; i < SIZE; ++i) {
-			table.add(null);
-		}
-		
-		size = 0;
-	}
-	
 	@Override
 	public int size() {
-		return size;
+		return 0;
 	}
 
 	@Override
 	public List<K> keySet() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<V> valueSet() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public V get(K key) {
-		if (contains(key)) {
-			LinkedList<Pair<K,V>> values = table.get(hashIndex(key));
-			
-			for(Pair<K,V> pair : values) {
-				if (pair.key.equals(key)) {
-					return pair.value;
-				}
-			}
-		}
-		
 		return null;
 	}
 
 	@Override
 	public boolean contains(K key) {
-		int index = hashIndex(key);
-		
-		if (table.get(index) == null || table.get(index).size() == 0) {
-			return false;
-		}
-		
-		return true;
-	}
-
-	private int hashIndex(K key) {
-		int hash = key.hashCode();
-		hash = (hash >= 0) ? hash : hash * (-1);
-		int index = hash % SIZE;
-		return index;
-	}
-	
-	@Override
-	public void insert(K key, V value) {
-		
-		int index = hashIndex(key);
-		
-		LinkedList<Pair<K, V>> values = table.get(index);
-		
-		if (values == null) {
-			values = new LinkedList<>();
-			table.set(index, values);
-		}
-		
-		Pair<K, V> pair = new Pair<>();
-		pair.key = key;
-		pair.value = value;
-		values.add(pair);
-		
-		++size;
-
-		if (first == null && last == null) {
-			first = pair;
-		} else if (last != null) {
-			last.next = pair;
-		}
-
-		pair.previous = last;
-		last = pair;
-	}
-
-	//TODO: Implement this method. It should return true if something was removed from the map.
-	@Override
-	public boolean remove(K key) {
-
-		int index = hashIndex(key);
-
-		LinkedList<Pair<K, V>> values = table.get(index);
-
-		if (values == null) {
-			return false;
-		}
-
-		for(Pair<K, V> pair : values) {
-
-			if (pair.key.equals(key)) {
-				values.remove(pair);
-				--size;
-				removeFromOrderList(pair);
-				return true;
-			}
-		}
-
 		return false;
 	}
 
-	private void removeFromOrderList(Pair<K, V> pair) {
+	@Override
+	public void insert(K key, V value) {
 
-		if (pair == first && pair == last) {
-			first = null;
-			last = null;
-			return;
-		}
-
-		if (pair == first) {
-			first = first.next;
-			return;
-		}
-
-		if (pair == last) {
-			last = last.previous;
-			return;
-		}
-
-		pair.previous.next = pair.next;
-		pair.next.previous = pair.previous;
-	}
-
-	public void clear() {
-
-		for(LinkedList<Pair<K, V>> values : table) {
-
-			if (values != null) {
-				values.clear();
-			}
-		}
-
-		for(int i=0; i < table.size(); ++i) {
-			table.set(i, null);
-		}
-
-		size = 0;
 	}
 
 	@Override
-	public String toString() {
-		final StringBuilder builder = new StringBuilder("[");
-
-		Pair<K, V> pair = first;
-
-		while (pair.next != null) {
-			builder.append(pair + ", ");
-			pair = pair.next;
-		}
-
-		builder.append(pair + "]");
-
-		return builder.toString();
-	}
-	
-	public static class Pair<K, V> {
-		public K key;
-		public V value;
-
-		Pair<K, V> next;
-		Pair<K, V> previous;
-
-		@Override
-		public String toString() {
-			return String.format("{%s: %s}", key.toString(), value.toString());
-		}
+	public boolean remove(K key) {
+		return false;
 	}
 }
--- src/com/musala/Main.java
+++ src/com/musala/Main.java
@@ -1,24 +1,5 @@
 package com.musala;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-
-interface Player {
-
-    static int count() {
-        return 5;
-    }
-
-    default void play() {
-        System.out.println("default");
-    }
-}
-
-class DVDPlayer implements Player {
-
-}
-
 public class Main {
 
     public static void main(String[] args) {
@@ -29,10 +10,6 @@ public class Main {
         numbers.insert("zero", 0);
         numbers.insert("eleven", 11);
         numbers.insert("five", 5);
-
-//        numbers.remove("eleven");
-//        numbers.remove("five");
-//        numbers.remove("zero");
 
         System.out.println(numbers.contains("seven"));
 
